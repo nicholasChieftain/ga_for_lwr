e35a1dd14a772da7a44b979147c16d17866d30a9
Николай Мальцев <nmmaltsev@outlook.com>
2020-11-28 13:11:25 +0500

    [+]base programm for alignment along the line

---
+++ ev3_utils.py
@@ -0,0 +1,39 @@
+from threading import Thread
+import rpyc
+
+
+class MotorThread(Thread):
+
+    conn = rpyc.classic.connect('192.168.0.11')
+    ev3dev2_motor = conn.modules['ev3dev2.motor']
+
+    def __init__(self):
+        self._motor_l_speed = 0
+        self._motor_r_speed = 0
+        self.motor_l = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_C)
+        self.motor_r = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_B)
+        self.running = True
+        Thread.__init__(self)
+
+    def run(self):
+        while self.running:
+            self.motor_l.on(self.ev3dev2_motor.SpeedDPS(-self._motor_l_speed))
+            self.motor_r.on(self.ev3dev2_motor.SpeedDPS(self._motor_l_speed))
+        self.motor_r.stop()
+        self.motor_l.stop()
+
+    @property
+    def motor_l_speed(self):
+        return self._motor_l_speed
+
+    @property
+    def motor_r_speed(self):
+        return self._motor_r_speed
+
+    @motor_l_speed.setter
+    def motor_l_speed(self, value):
+        self._motor_l_speed = value
+
+    @motor_r_speed.setter
+    def motor_r_speed(self, value):
+        self._motor_r_speed = value
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from camera_utils import CameraThread
+from ev3_utils import MotorThread
 
 camera_thread = CameraThread()
+motor_thread = MotorThread()
+
+threads = [camera_thread, motor_thread]
+
+for thread in threads:
+    thread.start()
+
+while True:
+    if camera_thread.centers_of_markers['green'][0] < camera_thread.coordinates_of_lines[1][1][0]:
+        motor_thread.motor_l_speed, motor_thread.motor_r_speed = -20, -20
+    elif camera_thread.centers_of_markers['green'][0] > camera_thread.coordinates_of_lines[1][1][0]:
+        motor_thread.motor_l_speed, motor_thread.motor_r_speed = 0, 0
